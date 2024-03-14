60af27442b9e88ae6e5243d1a69eb85b77de2e6e
fscek <filipscekic@gmail.com>
2024-03-14 23:55:34 +0100

    Update to include new 'read more' and menu button animations

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,15 +1,14 @@
 function toggleMenu() {
     var menuContent = document.getElementById("menuContent");
-    var menuIcon = document.getElementById("menuIcon");
-    var overlay = document.getElementById("pageOverlay"); // Get the overlay element
+    var overlay = document.getElementById("pageOverlay");
 
-    if (menuContent.style.display === "block") {
-        menuContent.style.display = "none";
-        overlay.style.display = "none"; // Hide the overlay when the menu is closed
-        menuIcon.src = "assets/images/hamburger-menu-open.png"; // Switch back to open icon
+    if (menuContent.classList.contains('active')) {
+        menuContent.classList.remove('active');
+        overlay.style.display = "none";
+        menuIcon.src = "assets/images/hamburger-menu-open.png";
     } else {
-        menuContent.style.display = "block";
-        overlay.style.display = "block"; // Show the overlay when the menu is open
-        menuIcon.src = "assets/images/hamburger-menu-close.png"; // Switch to close icon
+        menuContent.classList.add('active');
+        overlay.style.display = "block";
+        menuIcon.src = "assets/images/hamburger-menu-close.png";
     }
 }
--- assets/js/toggle-bio.js
+++ assets/js/toggle-bio.js
@@ -2,11 +2,13 @@ function toggleBio() {
     var moreText = document.getElementById("moreText");
     var readMoreBtn = document.getElementById("readMore");
 
-    if (moreText.style.display === "none") {
-        moreText.style.display = "inline";
-        readMoreBtn.innerHTML = "read less";
-    } else {
-        moreText.style.display = "none";
+    if (moreText.classList.contains("show")) {
+        moreText.classList.remove("show");
+        setTimeout(() => { moreText.style.display = "none"; }, 500); // Ensure display:none is applied after the transition
         readMoreBtn.innerHTML = "read more";
+    } else {
+        moreText.style.display = "block";
+        setTimeout(() => { moreText.classList.add("show"); }, 10); // Slight delay to ensure display:block is applied before adding opacity
+        readMoreBtn.innerHTML = "read less";
     }
 }
