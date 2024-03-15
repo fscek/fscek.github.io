fe6b4ca4db5054dae9b9d9b2784b6a7681222a27
fscek <filipscekic@gmail.com>
2024-03-15 19:04:35 +0100

    Refactor Music page scripts

--- assets/js/mixes-script.js
+++ assets/js/mixes-script.js
@@ -6,12 +6,12 @@ fetch('../assets/data/mixes.json')
         const mixItem = document.createElement('div');
         mixItem.className = 'mix-item';
 
-        let mixContent = `
-          <h3 class="mix-title">${mix.title}</h3>
-          <p class="mix-date">${mix.date}</p>
-          <img src="${mix.image}" alt="${mix.title}" class="mix-image">
-          <p>${mix.description}</p>
-          <a href="${mix.link}" target="_blank">Listen</a>`;
+        let mixContent = `<h3 class="mix-title">${mix.title}</h3>`;
+        if (mix.image) {
+            mixContent += `<img src="${mix.image}" alt="${mix.title}" class="mix-image" style="width:100%;max-width:600px;height:auto;">`;
+        }
+        mixContent += `<p>${mix.date}</p><p>${mix.description}</p>`;
+        mixContent += `<a href="${mix.link}" target="_blank" class="underline-link">Listen</a>`;
 
         mixItem.innerHTML = mixContent;
         mixesSection.appendChild(mixItem);
--- assets/js/releases-script.js
+++ assets/js/releases-script.js
@@ -6,15 +6,14 @@ fetch('../assets/data/releases.json')
         const releaseItem = document.createElement('div');
         releaseItem.className = 'release-item';
 
-        let releaseContent = `
-          <h3 class="release-title">${release.title}</h3>
-          <p class="release-date">${release.releaseDate}</p>
-          <img src="${release.image}" alt="${release.title}" class="release-image">
-          <p>${release.description}</p>
-          <div class="release-links">`;
+        let releaseContent = `<h3 class="release-title">${release.title}</h3>`;
+        if (release.image) {
+            releaseContent += `<img src="${release.image}" alt="${release.title}" class="release-image" style="width:100%;max-width:600px;height:auto;">`;
+        }
+        releaseContent += `<p>${release.releaseDate}</p><p>${release.description}</p><div class="release-links">`;
 
         release.links.forEach(link => {
-            releaseContent += `<a href="${link.url}" target="_blank">${link.platform}</a><br>`;
+            releaseContent += `<a href="${link.url}" target="_blank" class="underline-link">${link.platform}</a><br>`;
         });
 
         releaseContent += `</div>`;
