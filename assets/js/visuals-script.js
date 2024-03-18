349bea3b76d5945c795127440339662a9a3cc332
fscek <filipscekic@gmail.com>
2024-03-19 00:50:14 +0100

    Refactor the visuals page script and JSON to include support for multiple images

--- assets/js/visuals-script.js
+++ assets/js/visuals-script.js
@@ -1,33 +1,35 @@
 // Fetch and render visuals
 function fetchAndRenderVisuals() {
-    fetch('../assets/data/visuals.json')
-      .then(response => response.json())
-      .then(visuals => {
-        const visualsContainer = document.querySelector('.visuals-container');
-        
-        visuals.forEach(visual => {
-          const visualDiv = document.createElement('div');
-          visualDiv.className = 'visual-item';
-  
-          let visualHTML = `<h3 class="visual-title">${visual.title}</h3>`;
-          if (visual.image) {
-            visualHTML += `<img src="${visual.image}" alt="${visual.title}" class="visual-image">`;
-          }
-          if (visual.date) {
-            visualHTML += `<p>${visual.date}</p>`;
-          }
-          visualHTML += `<p class="visual-description">${visual.description}</p>`; // Use the class for padding
-          if (visual.links) {
-            visual.links.forEach(link => {
-              visualHTML += `<a href="${link.url}" target="_blank" class="visuals-link">${link.platform}</a><br>`;
-            });
-          }
-  
-          visualDiv.innerHTML = visualHTML;
-          visualsContainer.appendChild(visualDiv);
-        });
-      })
-      .catch(error => console.error('Error fetching visuals:', error));
+  fetch('../assets/data/visuals.json')
+    .then(response => response.json())
+    .then(visuals => {
+      const visualsContainer = document.querySelector('.visuals-container');
+      
+      visuals.forEach(visual => {
+        const visualDiv = document.createElement('div');
+        visualDiv.className = 'visual-item';
+
+        let visualHTML = `<h3 class="visual-title">${visual.title}</h3>`;
+        if (visual.images && visual.images.length) {
+          visual.images.forEach(image => {
+            visualHTML += `<img src="${image}" alt="${visual.title}" class="visual-image">`;
+          });
+        }
+        if (visual.date) {
+          visualHTML += `<p>${visual.date}</p>`;
+        }
+        visualHTML += `<p class="visual-description">${visual.description}</p>`; // Use the class for padding
+        if (visual.links) {
+          visual.links.forEach(link => {
+            visualHTML += `<a href="${link.url}" target="_blank" class="visuals-link">${link.platform}</a><br>`;
+          });
+        }
+
+        visualDiv.innerHTML = visualHTML;
+        visualsContainer.appendChild(visualDiv);
+      });
+    })
+    .catch(error => console.error('Error fetching visuals:', error));
 }
 
 // Initialize the script when the DOM is fully loaded
