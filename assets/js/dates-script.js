b63b0d71972e39ea1c2184d7cb2cbf6f156188ec
fscek <filipscekic@gmail.com>
2024-03-18 14:35:06 +0100

    Refactor to add better JSON parsing for the dates

--- assets/js/dates-script.js
+++ assets/js/dates-script.js
@@ -4,12 +4,20 @@ fetch('assets/data/dates.json')
     const datesList = document.getElementById('club-dates-list');
     dates.forEach(date => {
       const listItem = document.createElement('li');
+      // Check if clubnight is not empty and append a comma for separation
+      const clubnight = date.clubnight ? `${date.clubnight}${date.venue ? ' at ' : date.city ? ' - ' : ''}` : '';
+      const venue = date.venue ? `${date.venue}` : '';
+      const venueCitySeparator = date.venue && date.city ? ' - ' : '';
+      const city = date.city;
+
+      // Construct the display text
+      const displayText = `${date.date} - ${clubnight}${venue}${venueCitySeparator}${city}`;
 
       // If there's a URL, make the entire line a hyperlink
       if (date.url) {
-        listItem.innerHTML = `<a href="${date.url}" target="_blank" style="text-decoration: underline;">${date.date} - ${date.clubnight} at ${date.venue ? date.venue : ""} - ${date.city}</a>`;
+        listItem.innerHTML = `<a href="${date.url}" target="_blank" style="text-decoration: underline;">${displayText}</a>`;
       } else {
-        listItem.textContent = `${date.date} - ${date.clubnight}${date.venue ? ` at ${date.venue}` : ""} - ${date.city}`;
+        listItem.textContent = displayText;
       }
 
       datesList.appendChild(listItem);
